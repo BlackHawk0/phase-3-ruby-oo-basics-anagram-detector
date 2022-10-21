917e627fddce146e0370d7015cbe40c732ab35d4
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-10-21 07:31:27 +0300

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match array
+        array.select do |word|
+            (word.chars).sort == (@word.chars).sort
+        end
+    end
+end
+
+# Tests
+
+# allergy = Anagram.new('allergy')
+# matches = allergy.match(%w[gallery ballerina regally clergy largely leading])
+# puts matches
